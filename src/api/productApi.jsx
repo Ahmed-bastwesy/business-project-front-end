6f41b94da39b913ed6d9369ee7da95b61284d654
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-06-06 06:14:29 +0200

    update api

--- src/api/productApi.jsx
+++ src/api/productApi.jsx
@@ -3,5 +3,5 @@ import axios from 'axios';
 const url = 'http://127.0.0.1:8000/products';
 //#############  client dealing with cart ###########
 export const fetchProducts = () => axios.get(url);
-export const addProductToCard = (userId,productId) => axios.patch(`${url}/cart/${userId}/${productId}`);
+export const addProductToCard = (userId,productId) => axios.post(`${url}/cart/${userId}/${productId}`);
 export const deleteProductFromCard = (userId,productId) => axios.delete(`${url}/cart/${userId}/${productId}`);
