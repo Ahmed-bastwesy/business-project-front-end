9d34017ec74ca84a40b9a1af2c5e796815bfab0e
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-06-06 06:06:34 +0200

    edit api and edit actions --types--functions edit reducer

---
+++ src/storage/action/product/productCartActions.jsx
@@ -0,0 +1,35 @@
+import { FETCH_ALL,ADD_TO_CART,REMOVE_FROM_CART} from './types';
+
+import {fetchProducts,addProductToCard,deleteProductFromCard} from '../../../api/productApi';
+
+export const getProducts = () => async (dispatch) => {
+  try {
+    const { data } = await fetchProducts();
+
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
+export const addProduct = (id) => async (dispatch) => {
+    try {
+        await addProductToCard(id);
+        
+        dispatch({ type: ADD_TO_CART, payload: id });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+
+export const deleteProduct = (id) => async (dispatch) => {
+        try {
+          await deleteProductFromCard(id);
+      
+          dispatch({ type: REMOVE_FROM_CART, payload: id });
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
---
+++ src/storage/action/product/types.jsx
@@ -0,0 +1,3 @@
+export const FETCH_ALL = 'FETCH_ALL';
+export const ADD_TO_CART = 'add';
+export const REMOVE_FROM_CART ='delete'
